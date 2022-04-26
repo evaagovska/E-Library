553b894f8b162d4674d3dc93add1a727a0906ed0
Eva <evaagovska@gmail.com>
2022-04-26 14:31:51 +0200

    commit

--- frontend/src/repository/libraryRepository.js
+++ frontend/src/repository/libraryRepository.js
@@ -7,30 +7,33 @@ const LibraryService = {
     fetchCategories: () => {
         return axios.get("/books/categories")
     },
+    fetchAuthors: () => {
+        return axios.get("/authors")
+    },
     deleteBook: (id) => {
         return axios.delete(`/books/delete/${id}`);
     },
     addBook: (name, category, author, availableCopies) => {
         return axios.post("/books/add", {
             "name": name,
-            "category": category,
-            "author": author,
-            "availableCopies": availableCopies
+            "category": parseInt(category),
+            "author": parseInt(author),
+            "availableCopies": parseInt(availableCopies)
         });
     },
     editBook: (id, name, category, author, availableCopies) => {
         return axios.put(`/books/edit/${id}`, {
             "name": name,
-            "category": category,
-            "author": author,
-            "availableCopies": availableCopies
+            "category": parseInt(category),
+            "author": parseInt(author),
+            "availableCopies": parseInt(availableCopies)
         });
     },
     getBook: (id) => {
         return axios.get(`/books/${id}`);
     },
     markBook: (id) => {
-        return axios.put(`/books/mark/${id}`);
+        return axios.post(`/books/mark/${id}`);
     }
 }
 
